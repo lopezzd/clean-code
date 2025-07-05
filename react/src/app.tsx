01320140e8963b7683e4014e91501b068dbe1e98
João Henrique - Linux <joaohenriquelopesdiv@gmail.com>
2025-07-05 15:51:25 -0300

    Feat: Render condicional

--- react/src/app.tsx
+++ react/src/app.tsx
@@ -1,6 +1,7 @@
 import { useState, FormEvent } from "react";
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
+import * as Input from "./components/Input";
 
 function App() {
   const [tasks, setTasks] = useState<string[]>([]);
@@ -17,18 +18,31 @@ function App() {
     }
   }
 
+  const isTodoListEmpty = tasks.length === 0;
+
   return (
     <>
-      <Header onCreateTask={()=>{console.log("Task created")}}/>
+      <Header
+        onCreateTask={() => {
+          console.log("Task created");
+        }}
+      />
       <div>
         <h1>Add Task</h1>
-
+        // Componentização do input
         <form onSubmit={handleAddTask}>
-          <input type="text" name="task" />
+          <Input.Root>
+            <Input.Label htmlFor="task" id="task" />
+            <Input.FormField />
+            <Input.ErrorMessage message="Erro" />
+          </Input.Root>
+
           <button type="submit">Add task</button>
         </form>
-
         <article>
+          //Render condicional para exibir mensagem quando a lista de tarefas está vazia
+          {isTodoListEmpty && <p>No tasks available</p>}
+
           <ul>
             {tasks.map((item, index) => (
               <li key={index}>{item}</li>
